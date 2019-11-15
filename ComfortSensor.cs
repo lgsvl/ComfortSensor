0857ed70df18c04cf0aa21ac4c110bd4adc6eb92
Mark Gerow <mark.gerow@lge.com>
2019-11-14 16:58:48 -0800

    Initial commit.

---
+++ ComfortSensor.cs
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2019 LG Electronics, Inc.
+ *
+ * This software contains code licensed as described in LICENSE.
+ *
+ */
+
+using SimpleJSON;
+using Simulator.Api;
+using Simulator.Bridge;
+using Simulator.Sensors;
+using Simulator.Sensors.UI;
+using Simulator.Utilities;
+using UnityEngine;
+
+namespace Simulator.Sensors
+{
+    [SensorType("Comfort", new System.Type[] { })]
+    public class ComfortSensor : SensorBase
+    {
+        Rigidbody rigidbody;
+
+        Vector3 position;
+        Vector3 velocity;
+        Vector3 accel;
+        Vector3 jerk;
+        [SensorParameter]
+        public float maxAccelAllowed;
+        [SensorParameter]
+        public float maxJerkAllowed;
+
+        Quaternion rotation;
+        float angularVelocity;
+        float angularAcceleration;
+        [SensorParameter]
+        public float maxAngularVelocityAllowed;
+        [SensorParameter]
+        public float maxAngularAccelerationAllowed;
+        [SensorParameter]
+        public float rollTolerance;
+        [SensorParameter]
+        public float minSlipVelocity;
+        [SensorParameter]
+        public float slipTolerance;
+        public float slip;
+
+        private void Start()
+        {
+            rigidbody = transform.parent.GetComponentInChildren<Rigidbody>();
+        }
+
+        public void FixedUpdate()
+        {
+            CalculateValues();
+
+            if (!IsWithinRange())
+            {
+                var jsonData = new JSONObject();
+                jsonData.Add("velocity", velocity.magnitude);
+                jsonData.Add("acceleration", accel.magnitude);
+                jsonData.Add("jerk", jerk.magnitude);
+                jsonData.Add("angularVelocity", angularVelocity);
+                jsonData.Add("angularAcceleration", angularAcceleration);
+                jsonData.Add("roll", transform.rotation.eulerAngles.z);
+                jsonData.Add("slip", slip);
+                if (ApiManager.Instance != null)
+                {
+                    ApiManager.Instance.AddCustom(transform.parent.gameObject, "comfort", jsonData);
+                }
+            }
+        }
+
+        public void CalculateValues()
+        {
+            Vector3 posDelta = rigidbody.velocity;
+            Vector3 velocityDelta = (posDelta - velocity) / Time.fixedDeltaTime;
+            Vector3 accelDelta = (velocityDelta - accel) / Time.fixedDeltaTime;
+            jerk = accelDelta;
+            accel = velocityDelta;
+            velocity = posDelta;
+            position = transform.position;
+            float angleDelta = Quaternion.Angle(rotation, transform.rotation) / Time.fixedDeltaTime;
+            float angularVelocityDelta = (angleDelta - angularVelocity) / Time.fixedDeltaTime;
+            angularAcceleration = angularVelocityDelta;
+            angularVelocity = angleDelta;
+            rotation = transform.rotation;
+            slip = Vector3.Angle(rigidbody.velocity, transform.forward);
+        }
+
+        public bool IsWithinRange()
+        {
+            if (maxAccelAllowed > 0 && accel.magnitude > maxAccelAllowed)
+            {
+                return false;
+            }
+
+            if (maxJerkAllowed > 0 && jerk.magnitude > maxJerkAllowed)
+            {
+                return false;
+            }
+
+            if (maxAngularVelocityAllowed > 0 && angularVelocity > maxAngularVelocityAllowed)
+            {
+                return false;
+            }
+
+            if (maxAngularAccelerationAllowed > 0 && angularAcceleration > maxAngularAccelerationAllowed)
+            {
+                return false;
+            }
+
+            float zAxis = transform.rotation.eulerAngles.z;
+            float roll = zAxis < 180 ? zAxis : Mathf.Abs(zAxis - 360);
+            if (rollTolerance > 0 && roll > rollTolerance)
+            {
+                return false;
+            }
+
+            if (minSlipVelocity > 0 && slipTolerance > 0 && velocity.magnitude > minSlipVelocity && slip > slipTolerance)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override void OnBridgeSetup(IBridge bridge)
+        {
+        }
+
+        public override void OnVisualize(Visualizer visualizer)
+        {
+        }
+
+        public override void OnVisualizeToggle(bool state)
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,54 @@
-# ComfortSensor
+# Sensor Plugins
 
-Basic Sensor Plugin Sensor Template Showing if certain Acceleration and Rotation values are out of acceptable ranges
+This repository contains the code and assets for a custom Sensor Plugin
+
+To use this Sensor Plugin, clone the Sensor Plugin repo into Assets/External/Sensors as Assets/External/Sensors/{PluginName} inside of your Simulator Unity Project
+
+To build the Sensor Plugin for use with the Simulator, navigate to the Simulator -> Build Sensors Unity Editor menu item. Clicking on it will build every custom sensor in the Assets/External/Sensors directory and will output built Sensor Plugin bundles to the AssetBundles/Sensors folder
+
+On simulation startup, the Simulator will load all custom Sensor Plugin bundles in AssetBundles/Sensors directory and will be a valid sensor in a vehicle's configuration JSON
+
+Each Sensor Plugin repo must contain a Unity prefab with the same name as the directory ({PluginName}.prefab) that must have a script that inherits from SensorBase on the root object of the prefab
+
+Custom sensors must have SensorType attribute which specifies the kind of sensor being implemented as well as the type of data that the sensor sends over the bridge. In addition, it must have SensorBase as the base class and must implement the OnBridgeSetup, OnVisualize, and OnVisualizeToggle methods.
+
+SensorBase in inherited from Unity's Monobehavior so any of the Messages can be used to control how and when the sensor collects data.
+
+# Comfort Sensor
+
+Comfort Sensor detects whether a vehicle's acceleration or rotation values are out of acceptable ranges
+
+maxAccelAllowed - Maximum m/s^2 allowed
+
+maxJerkAllowed - Maximum m/s^3 allowed
+
+maxAngularVelocityAllowed - Maximum deg/s allowed
+
+maxAngularAccelerationAllowed - Maximum deg/s^2 allowed
+
+rollTolerance - Maximum deg rotation on the x axis
+
+slipTolerance - Maximum deg difference between vehicle's velocity and vehicle's forward
+
+Sample sensor config JSON:
+
+```json
+{
+"type" : "Comfort",
+"name" : "Comfort Sensor",    
+"params": {
+      "maxAccelAllowed": 8,
+      "maxJerkAllowed": 4,
+      "maxAngularVelocityAllowed": 200,
+      "maxAngularAccelerationAllowed": 100,
+      "rollTolerance": 10,
+      "slipTolerance": 15
+    }
+}
+```
+
+## Copyright and License
+
+Copyright (c) 2019 LG Electronics, Inc.
+
+This software contains code licensed as described in LICENSE.
